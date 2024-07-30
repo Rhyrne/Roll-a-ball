948c0b185523a4da85b90f1471286e7a781a4fbf
Rhyrne <100185977+Rhyrne@users.noreply.github.com>
2024-07-30 17:33:27 +0300

    Roll a ball project has finished

---
+++ Assets/Scripts/Controllers/BallController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    public Rigidbody body;
+    public int counter;
+    public int coinCounts;
+
+    public Text coinCounterText;
+    public Text gameOverText;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 vector = new Vector3(horizontal, 0, vertical);
+        body.AddForce(vector*10);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+        counter++;
+        coinCounterText.text = "Puan:" + counter;
+        if (counter == coinCounts) 
+        {
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject ball;
+    public Vector3 vector;
+    void Start()
+    {
+        vector = transform.position - ball.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + vector;
+    }
+}
